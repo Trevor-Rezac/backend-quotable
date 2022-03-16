71751033c1954fab93ec632bc00b3c842d746b96
TrevorRezac <trev.rezac@gmail.com>
2022-03-16 15:47:26 -0700

    added pseudocode for reference and removed unused vars

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Profile = require('../lib/models/Profile');
 
 describe('quotable routes', () => {
   beforeEach(() => {
